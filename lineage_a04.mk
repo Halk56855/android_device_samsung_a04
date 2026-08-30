dc42500e25061fdc5ccf0c81e0c075f88dad00fc
Halk56855 <147908980+Halk56855@users.noreply.github.com>
2026-08-30 05:40:10 +0300

    Update lineage_a04.mk

--- lineage_a04.mk
+++ lineage_a04.mk
@@ -10,8 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from a04 device
 $(call inherit-product, device/samsung/a04/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Lineage stuff (تم تحديث المسار ليتوافق مع الهيكل الحديث)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 PRODUCT_DEVICE := a04
 PRODUCT_NAME := lineage_a04
@@ -21,6 +21,7 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
+# تحديث إعدادات البصمة والوصف لتتجنب التضارب مع نظام أندرويد 16 الجديد
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="a04xx-user 14 UP1A.231005.007 A045FXXSFEZE2 release-keys" \
-    BuildFingerprint=samsung/a04xx/a04:14/UP1A.231005.007/A045FXXSFEZE2:user/release-keys
+    BuildDesc="a04xx-user 16 BP1A.250512.014 A045FXXSFEZE2 release-keys" \
+    BuildFingerprint="samsung/a04xx/a04:16/BP1A.250512.014/A045FXXSFEZE2:user/release-keys"
